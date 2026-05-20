0619be37ff09b159defd8c7450f82332dc397754
JovanVito <jovanvitohd@gmail.com>
2026-05-20 12:40:36 +0700

    Update urls.py

--- queue_app/urls.py
+++ queue_app/urls.py
@@ -1,15 +1,14 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from queue_app import views # Memanggil views dari aplikasi queue_app kamu
 
 urlpatterns = [
+    # Rute bawaan Django Admin
+    path('admin/', admin.site.urls),
+
     # ================= RUTE HALAMAN WEB (TAMPILAN UI) =================
-    # Halaman utama (Layar TV Monitor) saat membuka https://iot-4-three.vercel.app/
     path('', views.index_view, name='index'), 
-    
-    # Halaman Kiosk Registrasi
     path('registrasi/', views.registrasi_view, name='registrasi'), 
-    
-    # Halaman Tiket di HP Pengunjung
     path('tiket-mobile/', views.mobile_view, name='mobile'), 
     # ==================================================================
 
@@ -18,10 +17,8 @@ urlpatterns = [
     path('api/antrean/status/', views.status_antrean, name='status_antrean'),
     path('api/antrean/panggil/', views.panggil_antrean, name='panggil_antrean'),
     
-    # Tambahan rute baru untuk fitur Kasir/Admin
     path('api/antrean/selesai/', views.selesai_antrean, name='selesai_antrean'),
     path('api/antrean/lewati/', views.lewati_antrean, name='lewati_antrean'),
-    
     path('api/antrean/reset/', views.reset_antrean, name='reset_antrean'), 
     
     # ======== RUTE BARU: UNTUK TABEL DI LAYAR TV ========
